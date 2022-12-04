00b94a5570713b3cbdd183560e8b6e185a57a8e9
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-12-04 16:02:22 +0700

    added terraform s3 backend

--- infra/main.tf
+++ infra/main.tf
@@ -1,4 +1,9 @@
 terraform {
+  backend "s3" {
+    bucket = "fastapi-tf-bucket"
+    key    = "infra/terraform.tfstate"
+    region = "eu-west-1"
+  }
   required_providers {
     aws = {
       source  = "hashicorp/aws"
