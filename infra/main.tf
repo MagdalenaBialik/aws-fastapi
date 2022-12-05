c093c18debc94aefa013e17f5609e2ec39b45602
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-12-05 17:02:30 +0700

    added terraform variable

--- infra/main.tf
+++ infra/main.tf
@@ -40,8 +40,12 @@ resource "aws_lambda_function" "fastapi" {
   role = aws_iam_role.fastapi_role.arn
 
   s3_bucket = "fastapi-artifacts"
-  s3_key    = "api.zip"
+  s3_key    = "${var.file_hash}.zip"
 
   handler = "main.handler"
   runtime = "python3.8"
+}
+
+variable "file_hash" {
+  type = string
 }
