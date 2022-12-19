187b430132ac29c65ddc64507177bf284cf20511
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-12-19 20:09:17 +0700

    added policy documents to data.tf

--- infra/data.tf
+++ infra/data.tf
@@ -16,3 +16,14 @@ data "aws_iam_policy_document" "dynamodb_policy_document" {
     effect = "Allow"
   }
 }
+
+data "aws_iam_policy_document" "function_logging_policy_document" {
+  statement {
+    actions = [
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
+    ]
+    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:${aws_cloudwatch_log_group.lambda_logs.name}"]
+    effect    = "Allow"
+  }
+}
--- infra/main.tf
+++ infra/main.tf
@@ -34,31 +34,18 @@ resource "aws_iam_role" "fastapi_role" {
 }
 
 resource "aws_iam_policy" "function_logging_policy" {
-  name = "function-logging-policy"
-  policy = jsonencode({
-    "Version" : "2012-10-17",
-    "Statement" : [
-      {
-        Action : [
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
-        ],
-        Effect : "Allow",
-        Resource : "arn:aws:logs:*:*:*"
-      }
-    ]
-  })
-}
-
-resource "aws_iam_policy" "dynamodb_policy" {
-  name   = "dynamodb_policy"
-  policy = data.aws_iam_policy_document.dynamodb_policy_document.json
-
+  name   = "function-logging-policy"
+  policy = data.aws_iam_policy_document.function_logging_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "function_logging_policy_attachment" {
   role       = aws_iam_role.fastapi_role.id
   policy_arn = aws_iam_policy.function_logging_policy.arn
+}
+
+resource "aws_iam_policy" "dynamodb_policy" {
+  name   = "dynamodb_policy"
+  policy = data.aws_iam_policy_document.dynamodb_policy_document.json
 }
 
 resource "aws_iam_role_policy_attachment" "dynamodb_policy_attachment" {
