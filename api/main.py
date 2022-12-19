2ef2d485aa41936fc21d6487650912ecd8aecb29
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-12-19 18:06:59 +0700

    added module dynamodb under api

--- api/main.py
+++ api/main.py
@@ -1,15 +1,14 @@
 import os
 
-# import boto3
+import boto3
+from dynamodb.dynamodb import DynamodbDao
 from fastapi import FastAPI
 from mangum import Mangum
 
-# from api.dynamodb import DynamodbDao
-
 app = FastAPI()
 
-# dynamodb_client = boto3.client(service_name="dynamodb", region_name="eu-west-1")
-# dynamodb_dao = DynamodbDao(dynamodb_client=dynamodb_client)
+dynamodb_client = boto3.client(service_name="dynamodb", region_name="eu-west-1")
+dynamodb_dao = DynamodbDao(dynamodb_client=dynamodb_client)
 
 
 @app.get("/")
@@ -22,9 +21,9 @@ def variable():
     return os.environ.get("DYNAMODB_TABLE_NAME", "fastapi_table")
 
 
-# @app.get("/attraction")
-# def attraction(city, attraction_name):
-#     return dynamodb_dao.put_attraction(city, attraction_name)
+@app.get("/attraction")
+def attraction(city, attraction_name):
+    return dynamodb_dao.put_attraction(city, attraction_name)
 
 
 handler = Mangum(app=app)
