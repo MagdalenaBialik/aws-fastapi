f7e4ebbda11ef0c76a98c038c0355f2c822b60ee
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-12-22 20:50:14 +0700

    updated workspace name

--- infra/main.tf
+++ infra/main.tf
@@ -7,12 +7,6 @@ terraform {
     }
   }
 
-  #  backend "s3" {
-  #    bucket = "fastapi-tf-bucket"
-  #    key    = "infra/terraform.tfstate"
-  #    region = "eu-west-1"
-  #  }
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
