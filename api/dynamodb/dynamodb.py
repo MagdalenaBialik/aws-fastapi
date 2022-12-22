a3112cb2e8b130986a4818cc35a113e15db93249
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-12-22 17:02:52 +0700

    updated dynamodb.py

--- api/dynamodb/dynamodb.py
+++ api/dynamodb/dynamodb.py
@@ -5,8 +5,12 @@ class DynamodbDao:
     def __init__(self, dynamodb_client):
         self.dynamodb_client = dynamodb_client
 
-    def put_attraction(self, city, attraction):
+    def put_attraction(self, country, city, attraction):
         self.dynamodb_client.put_item(
             TableName=os.environ["DYNAMODB_TABLE_NAME"],
-            Item={"PK": {"S": city}, "SK": {"S": "1"}, "Attraction": {"S": attraction}},
+            Item={
+                "PK": {"S": country},
+                "SK": {"S": city},
+                "Attraction": {"S": attraction},
+            },
         )
