ddd0ad75aa54968781362cec76330eb96f03cc21
MagdalenaBialik <magdalena.bialik@gmail.com>
2022-12-22 17:14:01 +0700

    added sort key to travel table

--- infra/modules/dynamodb/main.tf
+++ infra/modules/dynamodb/main.tf
@@ -1,11 +1,17 @@
 resource "aws_dynamodb_table" "dynamodb_table" {
-  name           = "${var.app_name}-table"
+  name           = "${var.app_name}-travel-table"
   read_capacity  = 5
   write_capacity = 5
   hash_key       = "PK"
+  range_key      = "SK"
 
   attribute {
     name = "PK"
+    type = "S"
+  }
+
+  attribute {
+    name = "SK"
     type = "S"
   }
 }
