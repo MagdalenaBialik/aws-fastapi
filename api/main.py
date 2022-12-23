41acbd997798bb7cf65e819c1dc3698521cd5d71
Magdalena Bialik <64687726+MagdalenaBialik@users.noreply.github.com>
2022-12-23 20:09:28 +0700

    Dynamodb improvements (#17)
    * added terraform cloud workspace
    
    * added exception to get_attraction
    
    * added status code to put_attraction

--- api/main.py
+++ api/main.py
@@ -1,7 +1,8 @@
 import os
+from http import HTTPStatus
 
 import boto3
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
 from mangum import Mangum
 
 from api.dynamodb.dynamodb import DynamodbDao
@@ -22,14 +23,19 @@ def variable():
     return os.environ["DYNAMODB_TABLE_NAME"]
 
 
-@app.get("/attraction")
-def attraction(city, attraction_name):
-    return dynamodb_dao.put_attraction(city, attraction_name)
+@app.post("/put_attraction", status_code=201)
+def put_attraction(city, attraction_name):
+    dynamodb_dao.put_attraction(city, attraction_name)
+    return "Item successfully added"
 
 
 @app.get("/get_attraction")
 def get_attraction(city, attraction_name):
-    return dynamodb_dao.get_attraction(city, attraction_name)
+    response = dynamodb_dao.get_attraction(city, attraction_name)
+    if response is None:
+        raise HTTPException(status_code=HTTPStatus.NOT_FOUND, detail="Item not found")
+
+    return response
 
 
 @app.get("/get_attraction_by_city")
@@ -38,3 +44,8 @@ def get_attraction_by_city(city):
 
 
 handler = Mangum(app=app)
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, port=8080)
